15eed07b86dd8aa54fb3e509f1fb2797a80fa8aa
domininik <dominik.fijas+github@gmail.com>
2022-11-07 16:35:15 +0100

    Use ItemManager interface in Item contract

--- truffle/contracts/Item.sol
+++ truffle/contracts/Item.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.14;
 
 import "./ItemManager.sol";
 
+interface ItemManagerInterface {
+    function markItemAsPaid(uint index) external;
+}
+
 contract Item {
     uint public price;
     uint public index;
@@ -17,8 +21,6 @@ contract Item {
     receive() external payable {
         require(msg.value == price, "Price is not met");
 
-        (bool success, ) = address(manager).call{ value:msg.value }(abi.encodeWithSignature("markItemAsPaid(uint256)", index));
-
-        require(success, "Something went wrong");
+        ItemManagerInterface(address(manager)).markItemAsPaid(index);
     }
 }
