60f43b6df24da8dc0f59e73fe726639b90498284
domininik <dominik.fijas+github@gmail.com>
2023-07-02 20:51:11 +0200

    Add some more basic tests of ItemManager

--- truffle/test/ItemManager.test.js
+++ truffle/test/ItemManager.test.js
@@ -1,21 +1,48 @@
-const ItemManager = artifacts.require("ItemManager");
+const ItemManager = artifacts.require('ItemManager')
 
 contract('ItemManager', (accounts) => {
+  let itemManager
+  let owner
+
+  beforeEach(async() => {
+    itemManager = await ItemManager.new()
+    owner = accounts[0]
+  })
+
   it('assigns the contract owner', async() => {
-    const itemManager = await ItemManager.deployed();
-    assert.equal(accounts[0], await itemManager.owner.call());
+    assert.equal(owner, await itemManager.owner.call())
   })
 
-  it('creates new item data', async() => {
-    const itemManager = await ItemManager.deployed();
-    await itemManager.createItem('qwe', '100', { from: accounts[0] });
-    const itemData = await itemManager.items(0);
-    assert.equal('qwe', itemData.identifier);
+  it('creates new item data with proper data', async() => {
+    await itemManager.createItem('qwe', '100', { from: owner })
+    const itemData = await itemManager.items(0)
+    
+    assert.equal('qwe', itemData.identifier)
+    assert.equal(ItemManager.Status.Created, itemData.status)
   })
 
-  it('updates itemIndex which reflects total number of items', async() => {
-    const itemManager = await ItemManager.deployed();
-    const index = await itemManager.itemIndex.call();
-    assert.equal(1, index.toNumber());
+  it('updates itemIndex after creation which reflects total number of items', async() => {
+    await itemManager.createItem('qwe', '100', { from: owner })
+    const index = await itemManager.itemIndex.call()
+    
+    assert.equal(1, index.toNumber())
+  })
+
+  it('marks item as paid', async() => {
+    await itemManager.createItem('qwe', '100', { from: owner })
+    await itemManager.markItemAsPaid(0, { from: owner })
+    const itemData = await itemManager.items(0)
+    
+    assert.equal(ItemManager.Status.Paid, itemData.status)
+  })
+
+  it('marks item as delivered', async() => {
+    await itemManager.createItem('qwe', '100', { from: owner })
+    await itemManager.markItemAsPaid(0, { from: owner })
+    await itemManager.markItemAsDelivered(0, { from: owner })
+    const itemData = await itemManager.items(0)
+    
+    assert.equal(ItemManager.Status.Delivered, itemData.status)
   })
 })
+
